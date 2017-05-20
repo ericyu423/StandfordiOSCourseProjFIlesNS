274ddc91f72e8b075dbc8a9104f93a6b31ce1655
eric yu <ericyu423@erics-MacBook-Pro.local>
2017-05-20 15:18:24 -0700

    break

---
+++ CalculatorNoStoryboard/AnchorHelper.swift
@@ -0,0 +1,132 @@
+//
+//  AnchorHelper.swift
+//  CalculatorNoStoryboard
+//
+//  Created by eric yu on 5/20/17.
+//  Copyright © 2017 eric yu. All rights reserved.
+//
+
+import UIKit
+extension UIView {
+    
+    func anchorToTopView(viewOnTop v: UIView,paddingTop p: CGFloat){
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        self.topAnchor.constraint(equalTo: v.bottomAnchor, constant: p).isActive = true
+        self.leadingAnchor.constraint(equalTo: v.leadingAnchor, constant: 0).isActive = true
+        
+        self.trailingAnchor.constraint(equalTo: v.trailingAnchor, constant: 0).isActive = true
+        
+        self.heightAnchor.constraint(equalTo: v.heightAnchor, multiplier: 0)
+        
+    }
+    
+    func anchorToCenter(x: NSLayoutXAxisAnchor?, y: NSLayoutYAxisAnchor?,offsetX: CGFloat, offsetY: CGFloat,width: CGFloat, height: CGFloat){
+        translatesAutoresizingMaskIntoConstraints = false
+        if let x = x {
+            self.centerXAnchor.constraint(equalTo: x, constant: offsetX).isActive = true
+        }
+        if let y = y {
+            self.centerYAnchor.constraint(equalTo: y, constant: offsetY).isActive = true
+        }
+        if width != 0 {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        if height != 0 {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+    func anchorToVCEdege(type_self vc: UIViewController) {
+        translatesAutoresizingMaskIntoConstraints = false
+        //Note:  view.layoutMarginsGuide.trailingAnchor
+        //       provide about 8 standard space right edge
+        //       currently not used
+        //let margin = vc.view.layoutMarginsGuide
+        
+        self.topAnchor.constraint(equalTo: vc.topLayoutGuide.topAnchor, constant: 0).isActive = true
+        self.leadingAnchor.constraint(equalTo: vc.view.leadingAnchor, constant: 0).isActive = true
+        self.trailingAnchor.constraint(equalTo: vc.view.trailingAnchor, constant: 0).isActive = true
+        self.bottomAnchor.constraint(equalTo: vc.bottomLayoutGuide.bottomAnchor, constant: 0).isActive = true
+        
+    }
+    func anchorToViewEdeges(parentView: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        self.topAnchor.constraint(equalTo: parentView.topAnchor, constant: 0).isActive = true
+        self.leftAnchor.constraint(equalTo: parentView.leftAnchor, constant: 0).isActive = true
+        self.rightAnchor.constraint(equalTo: parentView.rightAnchor, constant: 0).isActive = true
+        self.bottomAnchor.constraint(equalTo: parentView.bottomAnchor, constant: 0).isActive = true
+        
+    }
+    
+    func anchor(top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?,  paddingTop: CGFloat, paddingLeft: CGFloat, paddingBottom: CGFloat, paddingRight: CGFloat, width: CGFloat, height: CGFloat) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+       
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if let left = left {
+            
+            
+            
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+            
+        }
+        
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if width != 0 {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if height != 0 {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+    func anchor(top: NSLayoutYAxisAnchor?, leading left: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing right: NSLayoutXAxisAnchor?,  paddingTop: CGFloat, paddingLeft: CGFloat, paddingBottom: CGFloat, paddingRight: CGFloat, width: CGFloat, height: CGFloat) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if let left = left {
+            
+            
+            
+            self.leadingAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+            
+        }
+        
+        if let right = right {
+            trailingAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if width != 0 {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if height != 0 {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+
+}
--- CalculatorNoStoryboard/ViewController.swift
+++ CalculatorNoStoryboard/ViewController.swift
@@ -8,20 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    //test commit
-
+class ViewController: UIViewController {    
+    let label:UILabel = {
+        let l = UILabel()
+        l.backgroundColor = .blue
+        return l
+    }()
+    
+    var button: [UIButton] = {
+        var b = [UIButton]()
+        for i in 1...10 {
+            let nb = UIButton()
+            nb.backgroundColor = .gray
+            nb.setTitle(String(i), for: .normal)
+            nb.addTarget(self, action: #selector(numberTapped), for: .touchUpInside)
+            b.append(nb)
+        }
+        return b
+    }()
+//MARK: -
+//MARK: ViewDidLoad
+//MARK: -
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        layoutViews()
+    }
+}
+//MARK: -
+//MARK: custom methods
+//MARK: -
+extension ViewController {
+     func numberTapped(_ sender: UIButton){
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+//MARK: - 
+//MARK: setup layout
+//MARK: -
+extension ViewController {
+    func layoutViews(){
+        view.addSubview(label)
+        let margin = view.layoutMargins
+        label.anchor(top: topLayoutGuide.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, paddingTop: margin.top, paddingLeft: margin.left, paddingBottom: 0, paddingRight: margin.right, width: 0, height: 100)
     }
-
-
 }
 
